3b38dfdb5fd08dae26a4452367046654902b4e38
lucabecci <beccibrian@gmail.com>
2021-10-10 22:25:57 -0300

    feat(): pod.tf for my server pod

---
+++ infra/server_pod.tf
@@ -0,0 +1,37 @@
+resource "kubernetes_pod" "server-pod" {
+  metadata {
+    name = "server"
+  }
+  
+  spec {
+    container {
+      image = "lucabecci/express-svc"
+      value = "server"
+      env {
+        name = "PORT"
+        value = 8080
+      }
+      env {
+        name = "NODE_ENV"
+        value = "prod"
+      }
+      env {
+        name = "NAMESPACE"
+        value = "Server_SVC"
+      }
+      port {
+        container_port = 8080
+      }
+
+      liveness_probe {
+        http_get {
+          path = "/"
+          port = 8080
+        }
+      }
+
+      initial_delay_seconds = 3
+      period_seconds = 3
+    }
+  }
+}
